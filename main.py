9eb176d92f9818d8f5f5d66455ddb1656a1a5264
davidkrejca <54067080+davidkrejca@users.noreply.github.com>
2019-08-14 12:26:44 +0200

    main.py
    Python is Easy - homework #1 - variables

---
+++ main.py
@@ -0,0 +1,17 @@
+# ABBA Knowing You Knowing Me
+Artist = "ABBA"
+Genre = "Pop"
+Album = "Arrival"
+Released = "1976"
+DurationInSeconds = "358"
+
+print(Artist)
+print(Genre)
+print(Album)
+print(Released)
+print(DurationInSeconds)
+
+"""
+ABBA
+Knowing You Knowing Me
+"""
