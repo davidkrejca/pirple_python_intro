bf123af5c78c644636bc7765549ff3432c5a78a4
davidkrejca <54067080+davidkrejca@users.noreply.github.com>
2019-08-16 15:31:55 +0200

    Add files via upload
    Functions #2

---
+++ functions.py
@@ -0,0 +1,13 @@
+# ABBA Knowing You Knowing Me #homework2, functions
+
+def Artist():
+    Artist = "ABBA"
+    return Artist
+
+def Genre():
+    Genre = "Pop"
+    return Genre
+
+def Year():
+    Released = "1976"
+    return Released
