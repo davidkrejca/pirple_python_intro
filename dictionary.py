caafdeebc2ea0a022eb5eb4e9a7e4e054bd0ae21
davidkrejca <54067080+davidkrejca@users.noreply.github.com>
2019-09-01 18:11:36 +0200

    dictionary
    refactored code h7

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# ABBA refactored homework7
+
+Dictionary = {"Artist":"ABBA","Gengre":"Pop","Album":"Arrival","Released":"1976","Duration":"358"}
+
+print(Dictionary)
+
+#"""
+#ABBA
+#Knowing You Knowing Me
+#"""
